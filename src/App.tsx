d04959ca5e91cc7e1ab5f8d301e526209d5e897d
0xthk <kf013099@gmail.com>
2022-04-01 17:02:33 +0800

    fix: remove hardhat from readOnlyUrls

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ const config: Config = {
   readOnlyChainId: ChainId.Polygon,
   readOnlyUrls: {
     [ChainId.Polygon]: 'https://polygon-rpc.com',
-    [ChainId.Hardhat]: 'http://127.0.0.1:8545',
   },
   multicallAddresses: {
     [ChainId.Hardhat]: '0x11ce4B23bD875D7F5C6a31084f55fDe1e9A87507',
