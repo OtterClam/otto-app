c6ded41f8c229ee12ccdf8181c285700db042eaa
0xthk <kf013099@gmail.com>
2022-04-16 11:53:42 +0800

    feat: add banner to wallet selector

--- src/components/WalletSelector/index.tsx
+++ src/components/WalletSelector/index.tsx
@@ -6,10 +6,11 @@ import { useDispatch, useSelector } from 'react-redux'
 import styled from 'styled-components/macro'
 import { ContentLarge } from 'styles/typography'
 import CoinbaseWalletIcon from 'assets/wallets/coinbase.png'
-import metamask from '../assets/wallets/metamask.jpg'
-import walletConnect from '../assets/wallets/walletconnect.jpg'
-import { selectConnectingWallet, walletConnected } from '../store/uiSlice'
-import Popup from './Popup'
+import metamask from 'assets/wallets/metamask.jpg'
+import walletConnect from 'assets/wallets/walletconnect.jpg'
+import { selectConnectingWallet, walletConnected } from '../../store/uiSlice'
+import Popup from '../Popup'
+import Banner from './wallet.png'
 
 const CoinbaseWallet = new WalletLinkConnector({
   url: 'https://polygon-rpc.com',
@@ -24,19 +25,27 @@ export const walletConnectConnector = new WalletConnectConnector({
   qrcode: true,
 })
 
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`
+
+const StyledBanner = styled.img`
+  width: 180px;
+`
+
 const Option = styled.button`
   width: 100%;
+  height: 70px;
   display: flex;
   align-items: center;
-  margin: 0.7rem 0;
   background: white;
   border: 4px solid ${({ theme }) => theme.colors.lightGray300};
   border-radius: 10px;
 
-  padding: 0.9rem 1.2rem;
-  @media (max-width: 600px) {
-    padding: 0.8rem 1.6rem;
-  }
+  padding: 0 20px;
 `
 
 const Name = styled(ContentLarge)`
@@ -46,7 +55,7 @@ const Name = styled(ContentLarge)`
 `
 
 const Icon = styled.img`
-  width: 3.2rem;
+  width: 36px;
 `
 
 const WalletSelector = (): JSX.Element => {
@@ -60,18 +69,21 @@ const WalletSelector = (): JSX.Element => {
       close={() => dispatch(walletConnected())}
       header={t('wallet_selector_head')}
     >
-      <Option onClick={() => activateBrowserWallet()}>
-        <Name>Metamask</Name>
-        <Icon src={metamask} alt="Metamask logo" />
-      </Option>
-      <Option onClick={() => activate(walletConnectConnector)}>
-        <Name>WalletConnect</Name>
-        <Icon src={walletConnect} alt="WalletConnect logo" />
-      </Option>
-      <Option onClick={() => activate(CoinbaseWallet)}>
-        <Name>Coinbase Wallet</Name>
-        <Icon src={CoinbaseWalletIcon} alt="Coinbase Wallet" />
-      </Option>
+      <StyledContainer>
+        <StyledBanner src={Banner} />
+        <Option onClick={() => activateBrowserWallet()}>
+          <Name>Metamask</Name>
+          <Icon src={metamask} alt="Metamask logo" />
+        </Option>
+        <Option onClick={() => activate(walletConnectConnector)}>
+          <Name>WalletConnect</Name>
+          <Icon src={walletConnect} alt="WalletConnect logo" />
+        </Option>
+        <Option onClick={() => activate(CoinbaseWallet)}>
+          <Name>Coinbase Wallet</Name>
+          <Icon src={CoinbaseWalletIcon} alt="Coinbase Wallet" />
+        </Option>
+      </StyledContainer>
     </Popup>
   )
 }
