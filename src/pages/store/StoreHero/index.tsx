1091c14fc7c45cabcd48e010dd682e6f2635cc67
0xthk <kf013099@gmail.com>
2022-04-30 18:01:46 +0800

    feat: rotate dialog for store hero

--- src/pages/store/StoreHero/index.tsx
+++ src/pages/store/StoreHero/index.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next'
 import styled, { keyframes } from 'styled-components/macro'
 import { ContentSmall, Headline } from 'styles/typography'
+import { useEffect, useState } from 'react'
 import Bg from './store_bg.jpg'
 import Otto1 from './store_otto1.png'
 import Otto2 from './store_otto2.png'
@@ -63,14 +64,24 @@ const StyledSubtitle = styled(ContentSmall)``
 
 export default function StoreHero() {
   const { t } = useTranslation()
+  const [state, setState] = useState(1)
+  useEffect(() => {
+    const interval = setInterval(() => setState(state => (state === 1 ? 2 : 1)), 3000)
+    return () => clearInterval(interval)
+  }, [])
   return (
     <StyledStoreHero>
       <StyledBg src={Bg} />
       <StyledOtto src={Otto1} delay={0} />
       <StyledOtto src={Otto2} delay={500} />
       <StyledDialog>
-        <StyledTitle as="h1">{t('store.hero')}</StyledTitle>
-        <StyledSubtitle as="p">{t('store.subtitle')}</StyledSubtitle>
+        {state === 1 && (
+          <>
+            <StyledTitle as="h1">{t('store.hero')}</StyledTitle>
+            <StyledSubtitle as="p">{t('store.subtitle_1')}</StyledSubtitle>
+          </>
+        )}
+        {state === 2 && <StyledSubtitle as="p">{t('store.subtitle_2')}</StyledSubtitle>}
       </StyledDialog>
     </StyledStoreHero>
   )
