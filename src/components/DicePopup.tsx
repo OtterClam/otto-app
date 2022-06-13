3ce074572d6860b362ddd7d2cda5e5b173d8b93b
Po-Ying Chen <poying.me@gmail.com>
2022-06-13 22:06:57 +0800

    feat: dice result animation

--- src/components/DicePopup.tsx
+++ src/components/DicePopup.tsx
@@ -23,9 +23,23 @@ import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
 import { hideDicePopup, selectOttoInTheHell } from 'store/uiSlice'
-import styled, { useTheme } from 'styled-components/macro'
+import styled, { keyframes, useTheme } from 'styled-components/macro'
 import { ContentExtraSmall, ContentLarge, ContentMedium, ContentSmall, Display3, Headline } from 'styles/typography'
 import StyledRichContent from './RichContent'
+
+const zoomInUp = keyframes`
+  from {
+    opacity: 0;
+    transform: scale3d(0.1, 0.1, 0.1) translate3d(0, 1000px, 0);
+    animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+  }
+
+  60% {
+    opacity: 1;
+    transform: scale3d(0.475, 0.475, 0.475) translate3d(0, -60px, 0);
+    animation-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1);
+  }
+`
 
 const StyledHellImage = styled.div`
   background: no-repeat center center / 180px 180px url(${hell});
@@ -103,6 +117,7 @@ const StyledResultImage = styled.div<StyledResultImageProps>`
   width: 420px;
   height: 420px;
   background: no-repeat center / cover url(${props => props.background});
+  animation: ${zoomInUp} 1.5s;
 `
 
 const StyledResultEffect = styled.span`
@@ -120,6 +135,7 @@ const StyledRankingBadge = styled.span`
 
 const StyledRibbonText = styled(RibbonText)`
   margin-top: -162px;
+  animation: ${zoomInUp} 2s;
 `
 
 interface StyledEventEffectProps {
@@ -264,9 +280,11 @@ function ResultState({ diceRoller, otto }: StateProps) {
         )}
         {!event.response && <StyledResultTitleInner>{event.event}</StyledResultTitleInner>}
       </Headline>
-      <StyledResultImage background={event.response ? bg : event.image} />
+      <StyledResultImage key={diceRoller.state} background={event.response ? bg : event.image} />
       {event.effects && (
-        <StyledRibbonText Typography={ContentExtraSmall}>BRS {numberWithSign(event.effects.brs)}</StyledRibbonText>
+        <StyledRibbonText key={diceRoller.state} Typography={ContentExtraSmall}>
+          BRS {numberWithSign(event.effects.brs)}
+        </StyledRibbonText>
       )}
       {event.type !== EventType.Question && event.effects && (
         <>
