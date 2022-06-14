c020adfc40ec59c1cbde6127440992dabce751ee
Po-Ying Chen <poying.me@gmail.com>
2022-06-14 18:17:23 +0800

    refactor: prevent app keep calling rpc

--- src/hooks/useRarityEpoch.ts
+++ src/hooks/useRarityEpoch.ts
@@ -1,6 +1,6 @@
 import { useOttoInfo } from 'contracts/views'
 import useQueryString from 'hooks/useQueryString'
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 
 const START_DATE = new Date('2022-05-23').valueOf()
 const EPOCH_LENGTH = 14 * 86400 * 1000 // 14 days
@@ -9,9 +9,9 @@ const epochSupply = [1699]
 
 export default function useRarityEpoch() {
   const epoch = Number(useQueryString().get('epoch') || -1)
-  const [now, setNow] = useState(Date.now())
+  const [latestEpoch, setLatestEpoch] = useState(epoch)
   const [totalSupply] = useOttoInfo()
-  const latestEpoch = useMemo(() => Math.floor((now - START_DATE) / EPOCH_LENGTH), [now])
+
   const epochEnd = START_DATE + (latestEpoch + 1) * EPOCH_LENGTH
   const isLatestEpoch = epoch === -1 || epoch === latestEpoch
   const hasPrevEpoch = (epoch === -1 || epoch > 0) && latestEpoch > 0
@@ -20,10 +20,13 @@ export default function useRarityEpoch() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setNow(Date.now())
+      const now = Date.now()
+      const latestEpoch = Math.floor((now - START_DATE) / EPOCH_LENGTH)
+      setLatestEpoch(latestEpoch)
     }, 1000)
     return () => clearInterval(interval)
   }, [])
+
   return {
     epoch,
     epochEnd,
