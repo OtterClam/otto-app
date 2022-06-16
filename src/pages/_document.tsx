a6efeb0cd58608c4f65ec76f16eac164f4015528
PYC <pyc@otterclam.finance>
2022-06-16 23:15:35 +0800

    feat: styled-components ssr

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,30 +1,56 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel="preconnect" href="https://www.googletagmanager.com" />
-        <link rel="preconnect" href="https://polygon-rpc.com" />
-        <link rel="preconnect" href="https://api.otterclam.finance" />
-        <link rel="preconnect" href="https://api.thegraph.com" />
-        <link rel="preconnect" href="https://cdn.jsdelivr.net" />
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
 
-        <link
-          rel="preload"
-          href="https://cdn.jsdelivr.net/gh/max32002/naikaifont@1.0/webfont/NaikaiFont-Regular-Lite.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin=""
-        />
-        <link rel="preload" href="/fonts/Pangolin-Regular.ttf" as="font" type="font/woff2" crossOrigin="" />
-        <link rel="preload" href="/ottoclick.mp3" as="audio" type="audio/mpeg" crossOrigin="" />
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
 
-        <script
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{
-            __html: `
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://www.googletagmanager.com" />
+          <link rel="preconnect" href="https://polygon-rpc.com" />
+          <link rel="preconnect" href="https://api.otterclam.finance" />
+          <link rel="preconnect" href="https://api.thegraph.com" />
+          <link rel="preconnect" href="https://cdn.jsdelivr.net" />
+
+          <link
+            rel="preload"
+            href="https://cdn.jsdelivr.net/gh/max32002/naikaifont@1.0/webfont/NaikaiFont-Regular-Lite.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin=""
+          />
+          <link rel="preload" href="/fonts/Pangolin-Regular.ttf" as="font" type="font/woff2" crossOrigin="" />
+          <link rel="preload" href="/ottoclick.mp3" as="audio" type="audio/mpeg" crossOrigin="" />
+
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
               ;(function (w, d, s, l, i) {
                 w[l] = w[l] || []
                 w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
@@ -36,42 +62,43 @@ export default function Document() {
                 f.parentNode.insertBefore(j, f)
               })(window, document, 'script', 'dataLayer', 'GTM-W4MP2GV')
             `,
-          }}
-        />
-        <title>Otto | The first official citizen of the Otter Kingdom</title>
-        <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
-        <meta property="og:title" content="Otto | The first official citizen of the Otter Kingdom" />
-        <meta
-          name="description"
-          content="Otto is the first official citizen of the Otter Kingdom. Each Otto is an ERC721 NFT with unique traits and attributes. These NFTs are not only irresistibly cute, but they also happen to be the main characters of OtterClam’s first adventure game – Ottopia."
-        />
-        <meta
-          property="og:description"
-          content=" Otto is the first official citizen of the Otter Kingdom. Each Otto is an ERC721 NFT with unique traits and attributes. These NFTs are not only irresistibly cute, but they also happen to be the main characters of OtterClam’s first adventure game – Ottopia."
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="/og.jpg" />
-        <link rel="apple-touch-icon" href="/logo192.png" />
-        <link rel="manifest" href="/manifest.json" />
-      </Head>
-      <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-W4MP2GV"
-            height="0"
-            width="0"
-            style={{
-              display: 'none',
-              visibility: 'hidden',
             }}
           />
-        </noscript>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+          <title>Otto | The first official citizen of the Otter Kingdom</title>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#000000" />
+          <meta property="og:title" content="Otto | The first official citizen of the Otter Kingdom" />
+          <meta
+            name="description"
+            content="Otto is the first official citizen of the Otter Kingdom. Each Otto is an ERC721 NFT with unique traits and attributes. These NFTs are not only irresistibly cute, but they also happen to be the main characters of OtterClam’s first adventure game – Ottopia."
+          />
+          <meta
+            property="og:description"
+            content=" Otto is the first official citizen of the Otter Kingdom. Each Otto is an ERC721 NFT with unique traits and attributes. These NFTs are not only irresistibly cute, but they also happen to be the main characters of OtterClam’s first adventure game – Ottopia."
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="/og.jpg" />
+          <link rel="apple-touch-icon" href="/logo192.png" />
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
+        <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-W4MP2GV"
+              height="0"
+              width="0"
+              style={{
+                display: 'none',
+                visibility: 'hidden',
+              }}
+            />
+          </noscript>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
