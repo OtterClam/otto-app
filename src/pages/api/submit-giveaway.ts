0587043fcebe16811edd220a0c2f26c5eb358c35
0xthk <kf013099@gmail.com>
2022-06-22 17:25:16 +0800

    fix: submit giveaway function

--- src/pages/api/submit-giveaway.ts
+++ src/pages/api/submit-giveaway.ts
@@ -1,9 +1,10 @@
+import console from 'console'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getApi } from 'pages/api/_libs/api'
 import redis from 'pages/api/_libs/redis'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { code, wallet, chainId } = JSON.parse(req.body || '{}')
+  const { code, wallet, chainId } = req.body
   const discordToken = req.headers.authorization?.split(' ')[1] || ''
   const part = req.headers.cookie?.split('twitter_token=') || []
   if (part.length < 2) {
@@ -32,6 +33,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     res.status(200).json(apiRes.data)
     return
   } catch (error: any) {
+    console.error(JSON.stringify(error))
     if (error.response) {
       res.status(error.response.status).json(error.response.data)
       return
