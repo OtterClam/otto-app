ee1d34c73ba472d985c3d241859dcd4c66ed4ba0
0xthk <kf013099@gmail.com>
2022-06-22 17:12:37 +0800

    fix: hardcode api endpoint

--- src/pages/api/_libs/api.ts
+++ src/pages/api/_libs/api.ts
@@ -2,9 +2,10 @@ import { ChainId } from '@usedapp/core'
 import axios from 'axios'
 
 export function getApi(chainId: number) {
-  let baseURL = process.env.NEXT_PUBLIC_API_ENDPOINT_MAINNET
+  // let baseURL = process.env.NEXT_PUBLIC_API_ENDPOINT_MAINNET
+  let baseURL = 'https://api.otterclam.finance/'
   if (chainId === ChainId.Mumbai) {
-    baseURL = process.env.NEXT_PUBLIC_API_ENDPOINT_MUMBAI
+    baseURL = process.env.NEXT_PUBLIC_API_ENDPOINT_MUMBAI || ''
   }
   return axios.create({ baseURL })
 }
