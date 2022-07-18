869bd8ee591dfdda98a529b00e56d5b2c331ccd9
PYC <pyc@otterclam.finance>
2022-07-18 10:29:05 +0800

    Revert "fix: ssr media query issue"
    This reverts commit eb599c2702781b05774a049a62b4ee7d8a81da46.

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,22 +1,10 @@
-import { useCallback, useLayoutEffect, useState } from 'react'
+import { useMediaQuery } from 'react-responsive'
+
 import { breakpoints } from 'styles/breakpoints'
 
-// TODO: should wrap the logic and state into a react context or put them into a react store
-export const useMediaQuery = (query: string) => {
-  const [matches, setMatches] = useState(false)
-
-  const handleChange = useCallback((e: MediaQueryListEvent) => setMatches(e.matches), [])
-
-  useLayoutEffect(() => {
-    window.matchMedia(query).addEventListener('change', handleChange)
-    setMatches(window.matchMedia(query).matches)
-    return () => window.matchMedia(query).removeEventListener('change', handleChange)
-  }, [query, handleChange])
-
-  return matches
-}
+export { useMediaQuery } from 'react-responsive'
 
 export const useBreakPoints = () => {
-  const isMobile = useMediaQuery(breakpoints.mobile)
+  const isMobile = useMediaQuery({ query: breakpoints.mobile })
   return { isMobile }
 }
