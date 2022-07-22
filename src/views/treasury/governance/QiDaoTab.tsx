7506e1082693b4321730324556791b33a5aa307f
abtheo <c.theo.kent@gmail.com>
2022-07-22 18:35:36 +0200

    Store governance progress

---
+++ src/views/treasury/governance/QiDaoTab.tsx
@@ -0,0 +1,24 @@
+import Button from 'components/Button'
+import SnapshotProposalGroup from 'components/SnapshotProposalGroup'
+import useQiDaoProposals from 'hooks/useQiDaoProposals'
+import { useTranslation } from 'next-i18next'
+import styled from 'styled-components'
+import { ContentSmall, Headline } from 'styles/typography'
+
+const StyledButton = styled(Button)``
+
+interface Props {
+  className?: string
+}
+
+export default function QiDaoTab({ className }: Props) {
+  const { t } = useTranslation('', { keyPrefix: 'treasury.governance' })
+  const { proposals } = useQiDaoProposals()
+  return (
+    <div className={className}>
+      <Headline as="h1">{t('welcome')}</Headline>
+      <ContentSmall as="p">{t('desc')}</ContentSmall>
+      <SnapshotProposalGroup proposals={proposals} />
+    </div>
+  )
+}
