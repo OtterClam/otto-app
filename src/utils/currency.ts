a8cb977b58fb0d7a7b39a8d91b17991f391d831c
PYC <pyc@otterclam.finance>
2022-08-04 11:32:46 +0800

    chore: upgrade next and react

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -2,14 +2,14 @@ import { BigNumber, BigNumberish, ethers } from 'ethers'
 import { trim } from 'helpers/trim'
 
 export const formatClamEthers = (value: BigNumber, decimal = 4) => trim(ethers.utils.formatUnits(value, 9), decimal)
-export const formatClamString = (number: string, appendClam: boolean = false) =>
+export const formatClamString = (number: string, appendClam = false) =>
   `${Intl.NumberFormat('en-US').format(Math.round(parseFloat(number)))} ${appendClam ? ' CLAM' : ''}`
 export const formatClamThousandsK = (number: string) =>
   `${Intl.NumberFormat('en-US').format(Math.round(parseFloat(number) / 1000))}k`
 
 export const formatUsdc = (value: BigNumberish, decimal = 4) => trim(ethers.utils.formatUnits(value, 6), decimal)
 
-export const formatUsd = (number: string | number, decimals: number = 0): string => {
+export const formatUsd = (number: string | number, decimals = 0): string => {
   return `${new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
