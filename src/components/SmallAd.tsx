a2f3895f916cf53483096515c1ac86f177113052
pyc <pyc@otterclam.finance>
2022-08-09 12:18:49 +0800

    feat: get small ad banner from airtable

---
+++ src/components/SmallAd.tsx
@@ -0,0 +1,33 @@
+import { Banner, BannerType } from 'models/Banner'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+const StyledContainer = styled.a`
+  display: inline-block;
+`
+
+export default function SmallAd({ className }: { className?: string }) {
+  const [ad, setAd] = useState<Banner>()
+
+  useEffect(() => {
+    fetch('/api/banners')
+      .then(res => res.json())
+      .then(data => data.find(({ type }: Banner) => type === BannerType.HomePageSmallAd))
+      .then(setAd)
+  }, [])
+
+  // suspense
+  if (!ad) {
+    return <div />
+  }
+
+  return (
+    <Link href={ad.link} passHref>
+      <StyledContainer className={className}>
+        {ad && <Image unoptimized src={ad.image} width={200} height={100} />}
+      </StyledContainer>
+    </Link>
+  )
+}
