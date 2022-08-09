5b6f324e6e10549f89ae9f5a0be7ddd74892797e
pyc <pyc@otterclam.finance>
2022-08-09 12:25:02 +0800

    feat: update home page bundle

--- src/views/home/HomePage.tsx
+++ src/views/home/HomePage.tsx
@@ -1,10 +1,14 @@
 import { useBreakpoints } from 'contexts/Breakpoints'
+import useAssetsBundles from 'hooks/useAssetsBundles'
+import { BundleName } from 'worker/consts'
 import DesktopHomePage from './DesktopHomePage'
 import MobileHomePage from './MobileHomePage'
 
 export default function HomePage() {
   const { isTablet, isMobile } = useBreakpoints()
   const dekstopVersion = !(isTablet || isMobile)
+
+  useAssetsBundles([BundleName.HomePage])
 
   if (dekstopVersion) {
     return <DesktopHomePage />
--- src/worker/assets-bundles/home.ts
+++ src/worker/assets-bundles/home.ts
@@ -2,11 +2,18 @@ import { BundleName } from '../consts'
 import { GeneralAssetsBundle } from './bundle'
 
 export const home = new GeneralAssetsBundle(BundleName.HomePage, [
-  'src/views/home/icons/portal-large.png',
-  'src/views/home/icons/portal.png',
-  'src/views/home/icons/buy-clam.png',
-  'src/views/home/icons/treasury.png',
-  'src/views/home/icons/dao.png',
-  'src/views/home/icons/whitepaper.png',
-  'src/views/home/icons/join-discord.png',
+  'src/views/home/Map/adventure.png',
+  'src/views/home/Map/bank.png',
+  'src/views/home/Map/city_center.jpg',
+  'src/views/home/Map/cloud_left.png',
+  'src/views/home/Map/cloud_right.png',
+  'src/views/home/Map/flag.png',
+  'src/views/home/Map/foundry.png',
+  'src/views/home/Map/governance.png',
+  'src/views/home/Map/leaderboard.png',
+  'src/views/home/Map/market.png',
+  'src/views/home/Map/mine.png',
+  'src/views/home/Map/mint.png',
+  'src/views/home/Map/pond.png',
+  'src/views/home/Map/store.png',
 ])
