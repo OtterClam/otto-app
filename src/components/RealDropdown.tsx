4fc4288f4063abfe255a60ef9ab57d5bfc872b0b
0xthk <kf013099@gmail.com>
2022-08-09 20:33:38 +0800

    feat: tweak dropdown animation

--- src/components/RealDropdown.tsx
+++ src/components/RealDropdown.tsx
@@ -1,15 +1,6 @@
 import { IS_SERVER } from 'constant'
 import useOnClickOutside from 'hooks/useOnClickOutside'
-import {
-  cloneElement,
-  isValidElement,
-  MouseEventHandler,
-  ReactChild,
-  ReactElement,
-  useMemo,
-  useRef,
-  useState,
-} from 'react'
+import { cloneElement, isValidElement, ReactChild, ReactElement, useMemo, useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
 import { CSSTransition } from 'react-transition-group'
 import styled from 'styled-components/macro'
@@ -19,26 +10,27 @@ const StyledContainer = styled.div<{ pos: { x: number; y: number } }>`
   z-index: var(--z-index-dropdown);
   left: ${({ pos }) => pos.x}px;
   top: ${({ pos }) => pos.y}px;
+  transform-origin: top center;
 
   &.slide-enter {
-    opacity: 0;
-    transform: translate(-12px);
+    opacity: 0.3;
+    transform: scaleY(0);
   }
 
   &.slide-enter-active {
     opacity: 1;
-    transform: translate(0);
+    transform: scaleY(1);
     transition: transform 0.2s, opacity 0.2s;
   }
 
   &.slide-exit {
     opacity: 1;
-    transform: translate(0);
+    transform: scaleY(1);
   }
 
   &.slide-exit-active {
-    opacity: 0;
-    transform: translate(12px);
+    opacity: 0.3;
+    transform: scaleY(0);
     transition: transform 0.2s, opacity 0.2s;
   }
 `
