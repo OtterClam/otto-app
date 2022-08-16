33d78a78a4712f854a9710c9e1fcfb60d6494d24
pyc <pyc@otterclam.finance>
2022-08-16 15:47:30 +0800

    feat: translations for foundry page

--- src/polyfill.ts
+++
@@ -1,7 +0,0 @@
-import { AbortController, abortableFetch } from 'abortcontroller-polyfill/dist/cjs-ponyfill'
-
-if (typeof window === 'undefined') {
-  ;(global as any).AbortController = AbortController
-  const { fetch: newFetch } = abortableFetch(fetch)
-  global.fetch = newFetch
-}
