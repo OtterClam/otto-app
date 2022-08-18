9914182d6249fb5dca8f679b44e21317f116aa59
0xthk <kf013099@gmail.com>
2022-08-18 11:41:58 +0800

    tmp: enable mumbai

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ const config: Config = {
   readOnlyChainId: ChainId.Polygon,
   readOnlyUrls: {
     [ChainId.Polygon]: 'https://polygon-rpc.com',
-    // [ChainId.Mumbai]: process.env.NEXT_PUBLIC_RPC_ENDPOINT_MUMBAI || '',
+    [ChainId.Mumbai]: process.env.NEXT_PUBLIC_RPC_ENDPOINT_MUMBAI || '',
     // [ChainId.Hardhat]: 'http://127.0.0.1:8545',
   },
   multicallAddresses: {
