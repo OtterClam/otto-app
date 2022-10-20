b27f363d6c71853d9ddb3e614013fa65b32f4ff1
0xthk <kf013099@gmail.com>
2022-10-20 22:31:20 +0900

    fix: query my otto use epoch -1

--- src/graphs/otto.ts
+++ src/graphs/otto.ts
@@ -96,7 +96,7 @@ export const GET_PORTAL = gql`
 
 export const LIST_MY_OTTOS = gql`
   query ListMyOttos($owner: Bytes!) {
-    ottos(where: { owner: $owner, portalStatus: SUMMONED }, orderBy: tokenId) {
+    ottos(where: { owner: $owner, portalStatus: SUMMONED, epoch: -1 }, orderBy: tokenId) {
       tokenId
     }
   }
