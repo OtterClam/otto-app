43b5565747c61479967c4d60b0db34a8ee69cdc0
pyc <pyc@otterclam.finance>
2022-11-01 20:48:51 +0800

    feat: add the bottom menu to all pages

--- src/components/GameMenu/items.ts
+++ src/components/GameMenu/items.ts
@@ -2,6 +2,7 @@ import itemsImage from './items.png'
 import missionsImage from './missions.png'
 import ottosImage from './ottos.png'
 import portalsImage from './portals.png'
+import adventureImage from './adventure.png'
 
 export interface Item {
   key: string
@@ -28,6 +29,11 @@ export const items = [
     key: 'ottos',
     image: ottosImage,
     link: '/my-ottos',
+  },
+  {
+    key: 'adventure',
+    image: adventureImage,
+    link: '/adventure',
   },
   {
     key: 'missions',
