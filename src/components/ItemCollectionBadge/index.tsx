ed96eebddc2e8ba69863cd1b77a1455ea364734c
pyc <pyc@otterclam.finance>
2022-12-04 20:49:40 +0800

    feat: gen3 icon

--- src/components/ItemCollectionBadge/index.tsx
+++ src/components/ItemCollectionBadge/index.tsx
@@ -2,10 +2,12 @@ import { TraitCollection } from 'models/Otto'
 import styled from 'styled-components/macro'
 import gen1Image from './gen1.svg'
 import gen2Image from './gen2.svg'
+import gen3Image from './gen3.svg'
 
 const images = {
   [TraitCollection.Genesis]: gen1Image,
   [TraitCollection.Second]: gen2Image,
+  [TraitCollection.Third]: gen3Image,
 }
 
 const StyledImage = styled.img`
