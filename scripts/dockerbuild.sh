d0cb6e59d1ea0f51cb2461cfb5857bbe82141fac
pyc <pyc@otterclam.finance>
2023-01-31 18:21:07 +0800

    ci: update settings

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -5,6 +5,5 @@ set -x
 PREFIX=$1
 SHORT_SHA=$(git rev-parse --short HEAD)
 TS=$(date +%s)
-#TAG=${IMAGE_NAME:-us.gcr.io/otterclam/otter-app}:$PREFIX-$SHORT_SHA-$TS
-TAG=${IMAGE_NAME:-us.gcr.io/otterclam/otter-app}:QQ
+TAG=${IMAGE_NAME:-us.gcr.io/otterclam/otter-app}:$PREFIX-$SHORT_SHA-$TS
 docker build --platform linux/amd64 -t "$TAG" .
