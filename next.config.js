feade76d4938a539d96cc2938ffa2d5aa4f8592d
pyc <pyc@otterclam.finance>
2023-02-03 14:03:30 +0800

    chore: install bundle analyzer

--- next.config.js
+++ next.config.js
@@ -4,60 +4,65 @@ const pkg = require('./package.json')
 const { i18n } = require('./next-i18next.config')
 const { AssetsManifestPlugin } = require('./webpack/assets-manifest')
 const withTM = require('next-transpile-modules')(['@usedapp/core'])
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const imageDomains = ['api.otterclam.finance', 'api-testnet.otterclam.finance', 'imgur.com']
 if (process.env.NODE_ENV === 'development') {
   imageDomains.push('localhost')
 }
 
-module.exports = withPWA(
-  withTM({
-    productionBrowserSourceMaps: false,
-    webpack: config => {
-      config.module.rules.push({
-        test: /\.mp3$/,
-        loader: 'file-loader',
-      })
-      config.plugins.push(
-        new DefinePlugin({
-          VERSION: pkg.version,
+module.exports = withBundleAnalyzer(
+  withPWA(
+    withTM({
+      productionBrowserSourceMaps: false,
+      webpack: config => {
+        config.module.rules.push({
+          test: /\.mp3$/,
+          loader: 'file-loader',
         })
-      )
-      config.plugins.push(new AssetsManifestPlugin())
-      return config
-    },
-    i18n,
-    pwa: {
-      dest: 'public',
-      scope: '/',
-      cacheStartUrl: false,
-      swSrc: './src/worker/index.ts',
-      disable: process.env.NODE_ENV === 'development',
-    },
-    images: {
-      domains: imageDomains,
-      formats: ['image/avif', 'image/webp'],
-    },
-    rewrites: async () => {
-      return [
-        {
-          source: '/health',
-          destination: '/api/health',
-        },
-      ]
-    },
-    headers: async () => {
-      return [
-        {
-          source: '/:all*(svg|je?pg|png|js|json|ttf|woff2|mp3)',
-          headers: [
-            {
-              key: 'Cache-Control',
-              value: 'public,max-age=86400',
-            },
-          ],
-        },
-      ]
-    },
-  })
+        config.plugins.push(
+          new DefinePlugin({
+            VERSION: pkg.version,
+          })
+        )
+        config.plugins.push(new AssetsManifestPlugin())
+        return config
+      },
+      i18n,
+      pwa: {
+        dest: 'public',
+        scope: '/',
+        cacheStartUrl: false,
+        swSrc: './src/worker/index.ts',
+        disable: process.env.NODE_ENV === 'development',
+      },
+      images: {
+        domains: imageDomains,
+        formats: ['image/avif', 'image/webp'],
+      },
+      rewrites: async () => {
+        return [
+          {
+            source: '/health',
+            destination: '/api/health',
+          },
+        ]
+      },
+      headers: async () => {
+        return [
+          {
+            source: '/:all*(svg|je?pg|png|js|json|ttf|woff2|mp3)',
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'public,max-age=86400',
+              },
+            ],
+          },
+        ]
+      },
+    })
+  )
 )
