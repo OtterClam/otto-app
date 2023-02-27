d0eac4978f3ebd5e2bbaa752c562accde4eff5d9
darkbrewery <theohawse@gmail.com>
2023-02-26 23:25:34 -0800

    Update index.tsx
    Skip the step of having to choose the otter again

--- src/components/AdventurePopup/ResultStep/index.tsx
+++ src/components/AdventurePopup/ResultStep/index.tsx
@@ -175,19 +175,17 @@ export default function ResultStep() {
   }
 
   const exploreAgainButton = otto?.adventureStatus !== AdventureOttoStatus.Resting && (
-    <Button
-      Typography={Headline}
-      onClick={() => {
-        closePopup();
-        openPopup(
-          undefined,
-          AdventurePopupStep.Map
-        );
-      }}
-    >
-      {t('explore_again_btn')}
-    </Button>
-  );
+  <Button
+    Typography={Headline}
+    onClick={() => {
+      setOtto(otto, true);
+      closePopup();
+      openPopup(undefined, AdventurePopupStep.Map);
+    }}
+  >
+    {t('explore_again_btn')}
+  </Button>
+);
 
   return (
     <StyledResultStep bg={location.bgImageBlack}>
