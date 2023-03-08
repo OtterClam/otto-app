ebdcde980c3157716480e54501c56f58652a49ad
darkbrewery <theohawse@gmail.com>
2023-03-07 18:10:33 -0800

    Update imagemin.mjs

--- scripts/imagemin.mjs
+++ scripts/imagemin.mjs
@@ -1,33 +1,42 @@
 // https://gist.github.com/tomchentw/19c4275bc2c2e89e18a616355a3cca88
 
-import fs from 'fs'
-import imagemin from 'imagemin'
-import gif from 'imagemin-gifsicle'
-import jpg from 'imagemin-jpegtran'
-import svg from 'imagemin-svgo'
-import png from 'imagemin-optipng'
+'use strict';
+const fs = require('fs');
+const imagemin = require('imagemin');
 
 const plugins = [
-  gif({ interlaced: true }),
-  jpg({ progressive: true }),
-  png({ optimizationLevel: 5 }),
-  svg({ plugins: [{ name: 'removeViewBox', active: false }] }),
-]
+  ['imagemin-gifsicle', {
+    interlaced: true,
+    optimizationLevel: 3 // set maximum optimization level (1-3)
+  }],
+  ['imagemin-jpegtran', {
+    progressive: true,
+    quality: 80 // set maximum quality value (0-100)
+  }],
+  ['imagemin-optipng', {
+    optimizationLevel: 5,
+  }],
+  ['imagemin-svgo', {
+    plugins: [
+      {removeViewBox: false},
+    ],
+  }],
+].map(it => require(it[0])(it[1]))
 
 const minifyFile = filename =>
   new Promise((resolve, reject) =>
     fs.readFile(filename, (err, data) => err ? reject(err) : resolve(data))
   )
-    .then(originalBuffer => imagemin
-      .buffer(originalBuffer, { plugins })
-      .then(minimizedBuffer => ({
-        // minimized: minimizedBuffer !== originalBuffer,
-        // originalSize: originalBuffer.length,
-        minimizedBuffer,
-      }))
-    ).then(({ minimizedBuffer }) => new Promise((resolve, reject) =>
-      fs.writeFile(filename, minimizedBuffer, err => err ? reject(err) : resolve())
-    ))
+  .then(originalBuffer => imagemin
+    .buffer(originalBuffer, { plugins })
+    .then(minimizedBuffer => ({
+      // minimized: minimizedBuffer !== originalBuffer,
+      // originalSize: originalBuffer.length,
+      minimizedBuffer,
+    }))
+  ).then(({ minimizedBuffer }) => new Promise((resolve, reject) =>
+    fs.writeFile(filename, minimizedBuffer, err => err ? reject(err) : resolve())
+  ))
 
 Promise.resolve(process.argv)
   .then(x => x.slice(2))
