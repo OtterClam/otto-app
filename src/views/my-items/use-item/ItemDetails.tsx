ec493d4f7789255446e35e2a84f1b1468236833f
eonwarped <eonwarped@gmail.com>
2023-03-27 11:33:11 -0400

    fix switching account refetch items logic, remove small debug

--- src/views/my-items/use-item/ItemDetails.tsx
+++ src/views/my-items/use-item/ItemDetails.tsx
@@ -199,7 +199,6 @@ export default memo(function ItemDetails({ item, onClose, onUse, className }: Pr
   } = item.metadata
   const equipped = Boolean(equippedBy)
   const [showTransferPopup, setShowTransferPopup] = useState(false)
-  console.log(item.metadata.labels, item.metadata.themeBoost)
 
   return (
     <StyledItemDetails className={className}>
